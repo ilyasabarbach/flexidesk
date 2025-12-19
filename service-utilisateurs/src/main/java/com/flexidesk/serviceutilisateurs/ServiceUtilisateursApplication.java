998916d569a22cd2c8f499606e5e88ad5bd3ebdf
unknown <ilyass.mambo@gmail.com>
2025-12-20 00:56:08 +0100

    creation de l'admin

--- service-utilisateurs/src/main/java/com/flexidesk/serviceutilisateurs/ServiceUtilisateursApplication.java
+++ service-utilisateurs/src/main/java/com/flexidesk/serviceutilisateurs/ServiceUtilisateursApplication.java
@@ -1,7 +1,12 @@
 package com.flexidesk.serviceutilisateurs;
 
+import com.flexidesk.serviceutilisateurs.model.User;
+import com.flexidesk.serviceutilisateurs.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ServiceUtilisateursApplication {
@@ -10,4 +15,16 @@ public class ServiceUtilisateursApplication {
 		SpringApplication.run(ServiceUtilisateursApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole("ROLE_ADMIN");
+                userRepository.save(admin);
+            }
+        };
+    }
 }
